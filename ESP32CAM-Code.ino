d7671a967d4e6e2763a484b8aad7f74c18b4003a
Ogbuagu Chibueze <74996517+Chibsssssss@users.noreply.github.com>
2024-08-07 03:14:45 +0100

    Update ESP32CAM-Code.ino VER7
    VER6 was a total failure. This should have been renamed VER6 but I'm tired

--- ESP32CAM-Code.ino
+++ ESP32CAM-Code.ino
@@ -1,150 +1,249 @@
-#include "esp_camera.h"
 #include <WiFi.h>
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
-#include "esp_http_server.h"
+#include <WebServer.h>
+#include <ESP32Servo.h>
+#include <NewPing.h>
+#include <DHT.h>
 
-const char* ssid = "CHIBS-MIFI";
-const char* password = "...........";
+#define DHTTYPE DHT11
 
+// Wi-Fi credentials
+const char* ssid = "Goldmindz_Electronics";
+const char* password = "Godisgreat24";
 
-void startCameraServer();
+//  *** Pin definitions ***
+
+// Motor control pins
+int IN1 = 13;
+int IN2 = 12; 
+int IN3 = 27;
+int IN4 = 14;
+int enA = 33;
+int enB = 15;
+
+int panServoPin = 25; // Servo motor pins
+int tiltServoPin = 26;
+
+// Ultrasonic Sensor Pins
+const int trigPinBack = 16;
+const int echoPinBack = 17;
+const int trigPinLeft = 18;
+const int echoPinLeft = 19;
+const int trigPinRight = 21;
+const int echoPinRight = 22;
+
+// Sensor Pins
+int ammoniaSensorPin = 34; // Analog input
+int metalSensorPin = 35;   // Analog input
+int temperatureSensorPin = 23; // DHT11 sensor pin
+int rainSensorPin = 32;    // Analog input
+
+// Radio Frequency Detector
+int radioFrequencyLevel_3 = 4;
+
+// Initialize NewPing objects for each ultrasonic sensor
+NewPing sonarBack(trigPinBack, echoPinBack, 200);
+NewPing sonarLeft(trigPinLeft, echoPinLeft, 200);
+NewPing sonarRight(trigPinRight, echoPinRight, 200);
+
+DHT dht(temperatureSensorPin, DHTTYPE);
+
+const int ledPin = 2; // LED pin
+
+Servo panServo;
+Servo tiltServo;
+
+int speedValue = 255;
+bool ledState = false;
+
+WebServer server(80);
 
 void setup() {
   Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
-
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = 5;
-  config.pin_d1 = 18;
-  config.pin_d2 = 19;
-  config.pin_d3 = 21;
-  config.pin_d4 = 36;
-  config.pin_d5 = 39;
-  config.pin_d6 = 34;
-  config.pin_d7 = 35;
-  config.pin_xclk = 0;
-  config.pin_pclk = 22;
-  config.pin_vsync = 25;
-  config.pin_href = 23;
-  config.pin_sscb_sda = 26;
-  config.pin_sscb_scl = 27;
-  config.pin_pwdn = 32;
-  config.pin_reset = -1;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_SVGA;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-
-  // Lower the resolution to reduce latency
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_QVGA;  // 320x240
-    config.jpeg_quality = 12;            // Lower quality for faster streaming
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_QVGA;  // 320x240
-    config.jpeg_quality = 12;            // Lower quality for faster streaming
-    config.fb_count = 1;
-  }
-
-  // Camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-
-  // Connect to Wi-Fi
+  dht.begin();
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
+  Serial.print("WiFi connected at IP: ");
+  Serial.println(WiFi.localIP());
 
-  Serial.println("WiFi connected");
-  Serial.print("Camera Stream Ready! Go to: http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
+  server.on("/", handleRoot);
+  server.on("/move", handleMove);
+  server.on("/servo", handleServo);
+  server.on("/speed", handleSpeed);
+  server.on("/toggleLED", handleToggleLED);
+  server.on("/sensor_data", handleSensorData);
+  server.begin();
 
-  startCameraServer();
+  // Initialize motor control pins
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+
+  // Initialize servos
+  panServo.attach(panServoPin);
+  tiltServo.attach(tiltServoPin);
+
+  // Initialize sensors
+  pinMode(ammoniaSensorPin, INPUT);
+  pinMode(metalSensorPin, INPUT);
+  pinMode(temperatureSensorPin, INPUT);
+  pinMode(rainSensorPin, INPUT);
+
+  pinMode(trigPinBack, OUTPUT);
+  pinMode(echoPinBack, INPUT);
+  pinMode(trigPinLeft, OUTPUT);
+  pinMode(echoPinLeft, INPUT);
+  pinMode(trigPinRight, OUTPUT);
+  pinMode(echoPinRight, INPUT);
+
+  pinMode(radioFrequencyLevel_3, INPUT);
+
+  pinMode(ledPin, OUTPUT);
+
+  Serial.println("Setup complete");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  handleSensorData();
+  server.handleClient();
 }
 
-// Add this function to start camera server
-#include "esp_http_server.h"
-
-httpd_handle_t camera_httpd = NULL;
-
-static esp_err_t stream_handler(httpd_req_t *req){
-  camera_fb_t * fb = NULL;
-  esp_err_t res = ESP_OK;
-  size_t _jpg_buf_len = 0;
-  uint8_t * _jpg_buf = NULL;
-  char * part_buf[64];
-
-  res = httpd_resp_set_type(req, "multipart/x-mixed-replace; boundary=frame");
-  if(res != ESP_OK){
-    return res;
-  }
-
-  while(true){
-    fb = esp_camera_fb_get();
-    if (!fb) {
-      Serial.println("Camera capture failed");
-      res = ESP_FAIL;
-    } else {
-      if(fb->format != PIXFORMAT_JPEG){
-        bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-        if(!jpeg_converted){
-          Serial.println("JPEG compression failed");
-          esp_camera_fb_return(fb);
-          res = ESP_FAIL;
-        }
-      } else {
-        _jpg_buf_len = fb->len;
-        _jpg_buf = fb->buf;
-      }
-    }
-    if(res == ESP_OK){
-      size_t hlen = snprintf((char *)part_buf, 64, "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n", _jpg_buf_len);
-      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, "\r\n--frame\r\n", 12);
-    }
-    if(fb->format != PIXFORMAT_JPEG){
-      free(_jpg_buf);
-    }
-    esp_camera_fb_return(fb);
-    if(res != ESP_OK){
-      break;
-    }
-  }
-  return res;
+void handleRoot() {
+  Serial.println("Root endpoint hit");
+  server.send(200, "text/html", "Hello, this is the main ESP32.");
 }
 
-void startCameraServer(){
-  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+void handleMove() {
+  String direction = server.arg("direction");
+  Serial.print("Move command received: ");
+  Serial.println(direction);
 
-  httpd_uri_t stream_uri = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = stream_handler,
-    .user_ctx  = NULL
-  };
-
-  if (httpd_start(&camera_httpd, &config) == ESP_OK) {
-    httpd_register_uri_handler(camera_httpd, &stream_uri);
+  if (direction == "forward") {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(enA, speedValue);
+    analogWrite(enB, speedValue);
+    Serial.print("Moving forward with speed: ");
+    Serial.println(speedValue);
+  } else if (direction == "backward") {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(enA, speedValue);
+    analogWrite(enB, speedValue);
+    Serial.print("Moving backward with speed: ");
+    Serial.println(speedValue);
+  } else if (direction == "left") {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(enA, speedValue);
+    analogWrite(enB, speedValue);
+    Serial.print("Turning left with speed: ");
+    Serial.println(speedValue);
+  } else if (direction == "right") {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(enA, speedValue);
+    analogWrite(enB, speedValue);
+    Serial.print("Turning right with speed: ");
+    Serial.println(speedValue);
+  } else if (direction == "stop") {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    Serial.println("Stopping");
   }
+
+  server.send(200, "text/plain", "OK");
+}
+
+void handleServo() {
+  String servo = server.arg("servo");
+  int value = server.arg("value").toInt();
+  Serial.print("Servo command received: ");
+  Serial.print(servo);
+  Serial.print(" with value: ");
+  Serial.println(value);
+
+  if (servo == "pan") {
+    panServo.write(value);
+    Serial.print("Pan servo set to: ");
+    Serial.println(value);
+  } else if (servo == "tilt") {
+    tiltServo.write(value);
+    Serial.print("Tilt servo set to: ");
+    Serial.println(value);
+  }
+
+  server.send(200, "text/plain", "OK");
+}
+
+void handleSpeed() {
+  speedValue = server.arg("value").toInt();
+  Serial.print("Speed value set to: ");
+  Serial.println(speedValue);
+  server.send(200, "text/plain", "OK");
+}
+
+void handleToggleLED() {
+  ledState = !ledState;
+  digitalWrite(ledPin, ledState ? HIGH : LOW);
+  Serial.print("LED toggled to: ");
+  Serial.println(ledState ? "ON" : "OFF");
+  server.send(200, "text/plain", "OK");
+}
+
+void handleSensorData() {
+  int ammoniaValue = analogRead(ammoniaSensorPin);
+  int metalValue = digitalRead(metalSensorPin);
+  float temperatureValue = dht.readTemperature();
+  int rainValue = digitalRead(rainSensorPin);
+
+  float ammoniaPPM = (ammoniaValue / 4095.0) * 100; // Example conversion, adjust as needed
+  float temperature = temperatureValue; // Already in degrees Celsius
+
+  int distanceBack = sonarBack.ping_cm();
+  int distanceLeft = sonarLeft.ping_cm();
+  int distanceRight = sonarRight.ping_cm();
+
+  Serial.print("Ammonia: ");
+  Serial.print(ammoniaPPM);
+  Serial.print(" ppm, Metal: ");
+  Serial.print(metalValue);
+  Serial.print(", Temperature: ");
+  Serial.print(temperature);
+  Serial.print(" C, Rain: ");
+  Serial.print(rainValue);
+  Serial.print(", Distance Back: ");
+  Serial.print(distanceBack);
+  Serial.print(" cm, Distance Left: ");
+  Serial.print(distanceLeft);
+  Serial.print(" cm, Distance Right: ");
+  Serial.println(distanceRight);
+
+  String json = "{";
+  json += "\"ammonia\":" + String(ammoniaPPM) + ",";
+  json += "\"metal\":" + String(metalValue) + ",";
+  json += "\"temperature\":" + String(temperature) + ",";
+  json += "\"rain\":" + String(rainValue) + ",";
+  json += "\"distanceBack\":" + String(distanceBack) + ",";
+  json += "\"distanceLeft\":" + String(distanceLeft) + ",";
+  json += "\"distanceRight\":" + String(distanceRight);
+  json += "}";
+
+  server.send(200, "application/json", json);
 }
