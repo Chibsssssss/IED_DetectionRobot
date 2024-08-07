61e13d22eb85f03df9b289855fa507e75d908d60
Ogbuagu Chibueze <74996517+Chibsssssss@users.noreply.github.com>
2024-08-07 03:05:55 +0100

    Update ESP32Code.ino VER4
    Removed getDistance function- the delays were causing lags.
    Changed movement mechanism

--- ESP32Code.ino
+++ ESP32Code.ino
@@ -2,56 +2,96 @@
 #include <WebServer.h>
 #include <ESP32Servo.h>
 
-// WiFi credentials
+// Wi-Fi credentials
 const char* ssid = "Chibs";
 const char* password = "12345678";
 
-// Create WebServer object
-WebServer server(80);
+// Pin definitions
+const int leftMotorPin1 = 13;
+const int leftMotorPin2 = 12;
+const int rightMotorPin1 = 14;
+const int rightMotorPin2 = 27;
+const int panServoPin = 25;
+const int tiltServoPin = 26;
+const int speedPin = 2;  // Dummy pin for speed control
 
-// Servo objects
-Servo panServo;
-Servo tiltServo;
-int panPos = 90;
-int tiltPos = 90;
-int speed = 50;
-
-// Ultrasonic sensor pins
-const int trigFrontPin = 5;
-const int echoFrontPin = 18;
-const int trigBackPin = 19;
-const int echoBackPin = 21;
-const int trigLeftPin = 22;
-const int echoLeftPin = 23;
-const int trigRightPin = 32;
-const int echoRightPin = 33;
-
-// Sensor pins
 const int ammoniaSensorPin = 34;
 const int metalSensorPin = 35;
 const int temperatureSensorPin = 36;
 
-// Servo pins
-const int panServoPin = 13;
-const int tiltServoPin = 12;
+Servo panServo;
+Servo tiltServo;
+
+WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.on("/move", handleMove);
+  server.on("/servo", handleServo);
+  server.on("/speed", handleSpeed);
+  server.on("/sensor_data", handleSensorData);
+  server.begin();
+
+  // Initialize motors
+  pinMode(leftMotorPin1, OUTPUT);
+  pinMode(leftMotorPin2, OUTPUT);
+  pinMode(rightMotorPin1, OUTPUT);
+  pinMode(rightMotorPin2, OUTPUT);
+
+  // Initialize servos
+  panServo.attach(panServoPin);
+  tiltServo.attach(tiltServoPin);
+
+  // Initialize sensors
+  pinMode(ammoniaSensorPin, INPUT);
+  pinMode(metalSensorPin, INPUT);
+  pinMode(temperatureSensorPin, INPUT);
+}
+
+void loop() {
+  server.handleClient();
+}
 
 void handleRoot() {
-  server.send(200, "text/plain", "Main ESP32 is working");
+  server.send(200, "text/html", "Hello, this is the main ESP32.");
 }
 
 void handleMove() {
   String direction = server.arg("direction");
   if (direction == "forward") {
-    // Move forward
+    digitalWrite(leftMotorPin1, HIGH);
+    digitalWrite(leftMotorPin2, LOW);
+    digitalWrite(rightMotorPin1, HIGH);
+    digitalWrite(rightMotorPin2, LOW);
     Serial.println("F");
   } else if (direction == "backward") {
-    // Move backward
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, HIGH);
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, HIGH);
     Serial.println("B");
   } else if (direction == "left") {
-    // Move left
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, HIGH);
+    digitalWrite(rightMotorPin1, HIGH);
+    digitalWrite(rightMotorPin2, LOW);
     Serial.println("L");
   } else if (direction == "right") {
-    // Move right
+    digitalWrite(leftMotorPin1, HIGH);
+    digitalWrite(leftMotorPin2, LOW);
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, HIGH);
     Serial.println("R");
   }
   server.send(200, "text/plain", "OK");
@@ -69,79 +109,15 @@ void handleServo() {
 }
 
 void handleSpeed() {
-  speed = server.arg("value").toInt();
+  // Speed control code here
   server.send(200, "text/plain", "OK");
 }
 
 void handleSensorData() {
-  int ammonia = analogRead(ammoniaSensorPin);
-  int metal = digitalRead(metalSensorPin);
+  int ammoniaLevel = analogRead(ammoniaSensorPin);
+  int metalDetected = digitalRead(metalSensorPin);
   int temperature = analogRead(temperatureSensorPin);
 
-  String json = "{";
-  json += "\"ammonia\": " + String(ammonia) + ",";
-  json += "\"metal\": " + String(metal) + ",";
-  json += "\"temperature\": " + String(temperature) + ",";
-  json += "\"front\": " + String(getDistance(trigFrontPin, echoFrontPin)) + ",";
-  json += "\"back\": " + String(getDistance(trigBackPin, echoBackPin)) + ",";
-  json += "\"left\": " + String(getDistance(trigLeftPin, echoLeftPin)) + ",";
-  json += "\"right\": " + String(getDistance(trigRightPin, echoRightPin));
-  json += "}";
-  server.send(200, "application/json", json);
-}
-
-long getDistance(int trigPin, int echoPin) {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  long duration = pulseIn(echoPin, HIGH);
-  return duration * 0.034 / 2;
-}
-
-void setup() {
-  Serial.begin(115200);
-
-  // Set up Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
-  Serial.println(WiFi.localIP());
-
-  // Set up server routes
-  server.on("/", handleRoot);
-  server.on("/move", handleMove);
-  server.on("/servo", handleServo);
-  server.on("/speed", handleSpeed);
-  server.on("/sensor_data", handleSensorData);
-
-  server.begin();
-  Serial.println("HTTP server started");
-
-  // Attach servos
-  panServo.attach(panServoPin);
-  tiltServo.attach(tiltServoPin);
-
-  // Set up ultrasonic sensors
-  pinMode(trigFrontPin, OUTPUT);
-  pinMode(echoFrontPin, INPUT);
-  pinMode(trigBackPin, OUTPUT);
-  pinMode(echoBackPin, INPUT);
-  pinMode(trigLeftPin, OUTPUT);
-  pinMode(echoLeftPin, INPUT);
-  pinMode(trigRightPin, OUTPUT);
-  pinMode(echoRightPin, INPUT);
-
-  // Set up other sensors
-  pinMode(ammoniaSensorPin, INPUT);
-  pinMode(metalSensorPin, INPUT);
-  pinMode(temperatureSensorPin, INPUT);
-}
-
-void loop() {
-  server.handleClient();
+  String sensorData = "{\"ammonia\": " + String(ammoniaLevel) + ", \"metal\": " + String(metalDetected) + ", \"temperature\": " + String(temperature) + "}";
+  server.send(200, "application/json", sensorData);
 }
